2c4b54c9a13445d8afaaa70304dce0a9f8de4e85
LenaSofia <lenasofia51@gmail.com>
2022-01-10 14:42:02 -0300

    Agregué ejercicios sobre formato (tabla_informe) clase 3, me falta

---
+++ Notas/03_Datos/ejercicios/formato/tabla_informe.py
@@ -0,0 +1,67 @@
+# Ejercicio informe.py N°2.15
+
+import csv
+
+def leer_camion(nombre_archivo):
+    '''Lee los nombres, cantidad de cajones y precios de frutas y verduras de un archivo y guarda cada fila
+    de información como una tupla dentro de una lista (camion)'''
+    camion = []
+
+    with open(nombre_archivo, 'rt') as archivo:
+        filas = csv.reader(archivo)
+        encabezado = next(filas)
+        for posicion, fila in enumerate(filas):
+            record = dict(zip(encabezado, fila))
+            try:
+                lote = (record['nombre'], int(record['cajones']), float(record['precio']))
+                camion.append(lote)
+            except ValueError:
+                print('Faltan datos en la línea', posicion, 'del archivo.')
+    return camion
+
+camion = leer_camion('../../../Data/camion.csv')
+print(camion)
+
+
+def leer_precios(nombre_archivo):
+    """Toma un .csv con datos de frutas/verduras y su precio por kilo y convierte esos datos en un diccionario,
+    dónde la fruta/verdura es la clave y el precio es el valor"""
+
+    precios = dict()
+
+    with open(nombre_archivo, 'rt') as archivo:
+        filas = csv.reader(archivo)
+
+        for posicion, fila in enumerate(filas):
+            try:
+                precios[fila[0]] = fila[1]
+            except:
+                print('Faltan datos en la línea', posicion, 'del archivo.')
+
+    return precios
+
+
+precios_camion = leer_precios('../../../Data/precios.csv')
+print(precios_camion)
+
+def hacer_informe(lista_cajones, diccionario_precios):
+    '''Recibe una lista de cajones y un diccionario con precios como input y devuelve una lista de tuplas
+    (Nombre, Cajones, Precio, Cambio -"Cambio" es la variación respecto al precio cobrado por el productor-)'''
+
+    informe = []
+
+    encabezado = ' Nombre     Cajones     Precio     Cambio  '
+    informe = encabezado
+
+    lineas = '---------- ---------- ---------- ----------'
+    informe += lineas
+
+    '''for elemento in lista_cajones:
+        fila = f'{elemento[0]:<10s} {elemento[1]:>10d} {elemento[2]:>10.2f} ' \
+               f'{(float(precios_camion[elemento[0]]) - elemento[1]):>10.2f}'
+        informe += fila'''
+
+    return informe
+
+informe = hacer_informe(camion, precios_camion)
+print(informe)
